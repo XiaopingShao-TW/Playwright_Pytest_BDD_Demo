6875c8063026391afb4ccd97c9288b1717ac67f3
shao.xiaoping <shao.xiaoping@thoughtworks.com>
2024-01-26 22:19:29 +0800

    clear branch

--- step_definitions/conftest.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-import pytest
-from page_object.search import Search
-
-
-@pytest.fixture(scope='module')
-def search_page(page):
-    yield Search(page)
--- utils/time.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-
-def date_time_now(fmt="%Y%m%d %H%M%S"):
-    return datetime.datetime.now().strftime(fmt)
