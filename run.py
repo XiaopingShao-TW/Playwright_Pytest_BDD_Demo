be5bf70dd79b8e3f7b35e222086720486c6b8791
shao.xiaoping <shao.xiaoping@thoughtworks.com>
2024-01-29 11:05:59 +0800

    clear branch

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from utils.tools import move_environment_file, generate_allure_report
 cmd = 'cd ' + cm.base_dir
 os.system(cmd)
 # 运行标签为smoke的测试用例集
-cmd = 'pytest -s --alluredir=temp -m smoke'
+cmd = 'pytest -v --gherkin-terminal-reporter -s --color=yes --alluredir=temp -m smoke'
 os.system(cmd)
 # 生成测试结果报告到report目录下
 move_environment_file()
